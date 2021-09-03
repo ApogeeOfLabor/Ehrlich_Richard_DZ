0fd82bc1b02393a0c88a04b29e15c9e06e6afb3b
richard <happy.ehrlich@icloud.com>
2021-09-04 01:09:44 +0300

    убрал лишний код добавил упстых строк в код для лучшей читаемости

--- Ehrlich_Richard_Lesson1/task1.py
+++ Ehrlich_Richard_Lesson1/task1.py
@@ -7,18 +7,20 @@ unit = (
 
 
 def get_time_distance(unit_list, seconds):
+
     time_dict = {}
+
     for unit_name, sec in unit_list:
         units = seconds // sec
         time_dict[unit_name] = units
         if units:
             seconds -= units * sec
-        else:
-            continue
+
     result = ("{} д ".format(time_dict['day']) if time_dict['day'] else '') + \
              ("{} час ".format(time_dict['hour']) if time_dict['hour'] else '') + \
              ("{} мин ".format(time_dict['minute']) if time_dict['minute'] else '') + \
              ("{} сек ".format(time_dict['second']) if time_dict['second'] else '')
+
     return result
 
 
