e98ae8e04b093a2a5a4e2bbb97fbd135aa910876
richard <happy.ehrlich@icloud.com>
2021-09-09 02:29:35 +0300

    Task4 Done

--- Ehrlich_Richard_Lesson3/task4.py
+++ Ehrlich_Richard_Lesson3/task4.py
@@ -1,24 +1,23 @@
-
-
 def thesaurus_adv(*args):
-    humans_data = dict()
+    key_by_lastname = dict()
     for _, data in enumerate(args):
-        if data[str(data).rindex(' ') + 1:][0] not in humans_data:
-            humans_data[data[str(data).rindex(' ') + 1:][0]] = [data]
+        if data[data.rindex(' ') + 1:][0] not in key_by_lastname:
+            key_by_lastname[data[data.rindex(' ') + 1:][0]] = [data]
         else:
-            humans_data[data[str(data).rindex(' ') + 1:][0]].append(data)
-    print(humans_data)
-    names_database = dict()
-    for key, value in zip(humans_data.keys(), humans_data.values()):
+            key_by_lastname[data[data.rindex(' ') + 1:][0]].append(data)
+
+    key_by_name = dict()
+    for key, value in key_by_lastname.items():
         for index, name in enumerate(value):
-            if name[:name.index(' ')][0] not in names_database.keys():
+            if name[:name.index(' ')][0] not in key_by_name.keys():
                 if name[name.rindex(' ') + 1:][0] == key:
-                    names_database[name[:name.index(' ')][0]] = [name]
-            elif name[name.rindex(' ') + 1:][0] == key: # нужно смотреть не по key  а по фамилиям созданных словарей
-                names_database[name[:name.index(' ')][0]].append(name)
-        print(names_database)
-    # Илья Иванов заходит не туда потому-что он видит что по ключу такой словарь создан и соответствует начальному key
+                    key_by_name[name[:name.index(' ')][0]] = [name]
+            elif name[name.rindex(' ') + 1:][0] == key:
+                key_by_name[name[:name.index(' ')][0]].append(name)
+        key_by_lastname[key] = {keys: values for keys, values in key_by_name.items()}
+        key_by_name.clear()
+    return key_by_lastname
 
 
-
-print(thesaurus_adv("Иван Сергеев", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева"))
+if __name__ == '__main__':
+    print(thesaurus_adv("Иван Сергеев", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева"))
