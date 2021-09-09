3b92bca4b43d3ebd3943163b59c6ee31c6e15fe4
richard <happy.ehrlich@icloud.com>
2021-09-09 03:21:32 +0300

    Исправляем код под условие в задании.

--- Ehrlich_Richard_lesson2/task2_and_3.py
+++ Ehrlich_Richard_lesson2/task2_and_3.py
@@ -1,10 +1,14 @@
-temp_info = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-print(id(temp_info), *temp_info)
-for num in temp_info:
-    if num.isdigit():
-        temp_info[temp_info.index(num)] = ('"{}"'.format(num) if len(num) == 2 else '"{0:02d}"'.format(int(num)))
-    elif num[0] == '-' or num[0] == '+':
-        signed_number = list(num)
+weather_report = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+print(id(weather_report), *weather_report)
+number_index_list = list()
+for index, value in enumerate(weather_report):
+    if value.isdigit():
+        weather_report[index] = f'{format(int(value)):>02}'
+        number_index_list.append(index)
+    elif value[0] == '-' or value[0] == '+':
+        signed_number = list(value)
         signed_number[-1] = ('{:02d}'.format(int(signed_number[-1])) if len(signed_number) == 2 else signed_number[1:])
-        temp_info[temp_info.index(num)] = '"{}"'.format(''.join(signed_number))
-print(id(temp_info), *temp_info)
+        weather_report[index] = '{}'.format(''.join(signed_number))
+        number_index_list.append(index)
+
+print(id(weather_report), weather_report)
