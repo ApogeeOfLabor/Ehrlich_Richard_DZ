c1ae4e0e24ff47b86a8e9834ddccd32d57936472
richard <happy.ehrlich@icloud.com>
2021-09-16 02:44:03 +0300

    Lesson4 Task2 Done.

--- Ehrlich_Richard_Lesson4/task3.py
+++ Ehrlich_Richard_Lesson4/task3.py
@@ -1,8 +1,18 @@
-def currency_rates():
-    response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
-    return response.text
+def currency_rates(url, name_valute):
+    dom_data_valute = ET.fromstring(requests.get(url).text)
+
+    for country in dom_data_valute.findall('Valute'):
+        code_valute = country.find('CharCode').text
+        value_valute = country.find('Value').text
+
+        if code_valute.lower() == name_valute.lower():
+            return '.'.join(value_valute.split(','))
 
 
 if __name__ == '__main__':
     import requests
-    print(currency_rates(), sep='\n')
+    import xml.etree.ElementTree as ET
+
+    url = 'http://www.cbr.ru/scripts/XML_daily.asp'
+
+    print(currency_rates(url, 'eur'), sep='\n')
