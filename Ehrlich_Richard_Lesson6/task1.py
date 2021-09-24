1cc66608c9aa7fbeded6352f5b24608c3b43c5fc
Richard <happy.ehrlich@icloud.com>
2021-09-24 21:09:35 +0300

    Lesson6 task1 modify

--- Ehrlich_Richard_Lesson6/task1.py
+++ Ehrlich_Richard_Lesson6/task1.py
@@ -1,13 +1,20 @@
-import requests
-url = 'https://github.com/elastic/examples/raw/master/Common%20Data%20Formats/nginx_logs/nginx_logs'
-response = requests.get(url).text.split('\n')
-xlist = []
-for _, string in enumerate(response):
-    for string_item in string.split():
-        if string_item.strip('", -'):
-            xlist.append(string_item.strip('", -'))
-    ip = xlist[0] if string.split() else ''
-    wois_request = xlist[3] if string.split() else ''
-    domain_pointer = xlist[4] if string.split() else ''
-    print((ip, wois_request, domain_pointer))
-    xlist.clear()
+def get_info(raw_response):
+    for inner_string in raw_response.text.splitlines():
+        ip = re.search(r'\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}', inner_string)
+        req = re.search(r'[A-Z]{3,4}', inner_string)
+        dom = re.search(r'/\w*/\w*_\d{1,2}', inner_string)
+        yield ip.group(0), req.group(0), dom.group(0)
+
+
+if __name__ == '__main__':
+    import requests
+    import re
+
+    response = requests.get('https://github.com/elastic/examples/raw/master/Common%20Data%20Formats/nginx_logs/nginx_logs')
+
+    print(*get_info(response), sep='\n')
+    # task1 done
+
+    with open('modify_log_nginx.txt', 'w') as file:
+        for inner_tuple in [*get_info(response)]:
+            file.writelines(str(inner_tuple) + '\n')
