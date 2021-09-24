4a74195b62a06ac2cc0a75b3aff38feb8a6d4e50
richard <happy.ehrlich@icloud.com>
2021-09-24 23:42:28 +0300

    Lesson6 task2 оптимизация кода

--- Ehrlich_Richard_Lesson6/task2.py
+++ Ehrlich_Richard_Lesson6/task2.py
@@ -1,20 +1,16 @@
 def get_spammer():
     with open('modify_log_nginx.txt', 'r') as file:
-        log_list = [inner_string[1:-2].split() for inner_string in file.readlines()]
-        # обрезаем круглые скобки и если я правильно понял в конце обрезается перевод коретки,
-        # но почему-то он обрезается как один символ. Хотя визуально его вообще не было видно.
-        for idx, inner_list in enumerate(log_list):
-            for index, item_string in enumerate(inner_list):
-                inner_list[index] = item_string.strip(" ,'")
-            log_list[idx] = inner_list
-        # чистка строк и списков для возможности работы с ними
-        raw_dict = collections.Counter([get_ip[0] for get_ip in log_list])
-        max_step_spammer = max(raw_dict.values())
-        for key, value in raw_dict.items():
-            if raw_dict[key] == max_step_spammer:
+        ip_counter = collections.defaultdict(int)
+        for one_string in file.readlines():
+            ip_counter[one_string[one_string.find("'")+1: one_string.index("'", one_string.find("'")+1)]] += 1
+        max_step_spammer = max(ip_counter.values())
+        for key, value in ip_counter.items():
+            if ip_counter[key] == max_step_spammer:
                 return key, value
 
 
 if __name__ == '__main__':
     import collections
     print(get_spammer())
+    # чувствую, что напрофессиональном уровне это всё на много проще и красивее выглядит,
+    # но пока лучше не могу и то немного с твоей подсказкой:) - думаю в комитах это видно.
