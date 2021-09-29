f0dbc0b840d2df1a70cfbc035a884bbd340d4a99
Richard <happy.ehrlich@icloud.com>
2021-09-29 17:43:19 +0300

    Lesson6 task5 refactoring

--- Ehrlich_Richard_Lesson6/task4.py
+++ Ehrlich_Richard_Lesson6/task4.py
@@ -1,18 +1,12 @@
-def get_passport_data(file_name):
-    with open(file_name, 'r', encoding='utf-8') as f:
-        for string in f.readlines():
-            yield string
-
-
-def get_hobbies(file_name):
+def get_data(file_name):
     with open(file_name, 'r', encoding='utf-8') as f:
         for string in f.readlines():
             yield string
 
 
 def make_file(users_file, hobbies_file):
-    users_data = (item.strip('\n,') for item in get_passport_data(users_file) if item)
-    users_hobby = (item.strip('\n,') for item in get_hobbies(hobbies_file) if item)
+    users_data = (item.strip('\n,') for item in get_data(users_file) if item)
+    users_hobby = (item.strip('\n,') for item in get_data(hobbies_file) if item)
     for user_information, user_hobbies in zip(users_data, users_hobby):
         if user_information and user_hobbies:
             with open('users_hobby_for_task4.txt', 'a', encoding='utf-8') as f:
