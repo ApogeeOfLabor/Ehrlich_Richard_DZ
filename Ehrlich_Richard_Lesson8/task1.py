74456a29072bd6a0ba900ccfd3871b8384cc415c
Richard <happy.ehrlich@icloud.com>
2021-12-02 00:56:17 +0300

    Lesson8 task1

--- Ehrlich_Richard_Lesson8/task1.py
+++ Ehrlich_Richard_Lesson8/task1.py
@@ -1,8 +1,16 @@
-import re
+def qwe(mail):
+    # Немного схалявил) , но решил не списывать проверку email на валидность,
+    # а когда пойму как правильно сам напишу.
+    x = re.compile(r"(?:\w+[-.]*)+[^@]")
+    y = x.findall(mail)
+    user_data['username'] = y[0]
+    user_data['domain'] = y[1]
+    return user_data
 
-RE_DATE = re.compile(r'(?:\d{2}[./-]){2}\d{4}')
 
-txt = 'Погода 23.01.2021 была отличная! Зато за день до этого (22/01/2021) - очень холодно. ' \
-     'Надеемся, что 24-01-2021 будет без ветра.'
-
-print(RE_DATE.findall(txt))
+if __name__ == '__main__':
+    import re
+    user_data = {'username': '', 'domain': ''}
+    user_email = input('Enter email: ')
+    if user_email.count('@') == 1:
+        print(qwe(user_email))
